1bdea30600137281c919b25e6f3b38baeaec1ee3
fanmengjihua <156817867+fanmengjihua@users.noreply.github.com>
2026-01-03 22:33:34 +0800

    Fix sharp module error for Cloudflare Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import AutoImport from "astro-auto-import";
 import { defineConfig } from "astro/config";
 import remarkCollapse from "remark-collapse";
 import remarkToc from "remark-toc";
-import sharp from "sharp";
 import config from "./src/config/config.json";
 
 // https://astro.build/config
@@ -15,7 +14,6 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
-  image: { service: sharp() },
   vite: { plugins: [tailwindcss()] },
   output: "server",
   adapter: cloudflare(),
